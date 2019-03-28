34b7484ddf718f8e65e088a0d7b62aa3e23e34fa
Shahidul Shakib <shakibkhanrock@gmail.com>
2019-03-28 19:46:13 +0600

    added

--- practise4.py
+++
@@ -1,3 +0,0 @@
-str="ABC123"
-for x in range(1,len(str)+1):
-    print(str[:x])
--- stemmer(2nd).py
+++ stemmer(2nd).py
@@ -1,23 +1,27 @@
+########### File read
 F = open(r"C:\Users\Inception\PycharmProjects\FIRSTproject\data_corpus", 'r', encoding="utf8")
 plain_text = F.read()
 F2 = open(r"bibokti",'r', encoding="utf8")
 bibokti=F2.read()
 F3 = open(r"2nd_step_file",'r', encoding="utf8")
 bibokti2_1=F3.read()
+F4 = open(r"bochon",'r', encoding="utf8")
+bochon=F4.read()
+
+#############File spilit
 bibokti2=bibokti.split(" ")
 bibokti3=bibokti2_1.split(" ")
 plain_text2=plain_text.split(" ")
+bochon_spilit=bochon.split(" ")
 
-
-
+############## Extra variable declaration
 map={}
 mm=[]
 dari='া'
 
+
 for x in plain_text2:
     map[x]=0
-
-
 
 def place_dari(str):
     for x in mm:
@@ -52,13 +56,16 @@ def match(str,list3):
             match(str,list3)
     return str
 
+##############Main function
+
 list4=unique(bibokti2)
 list5=unique(bibokti3)
+list6=unique(bochon_spilit)
 
 for x in plain_text2:
-    str=match(x,list4);
-    str=sec_match(str,list5);
-    str = sec_match(str, list5);
+    str=match(x,list4)
+    str=sec_match(str,list5)
+    str = sec_match(str, list5)
     print(str)
 
 
